f0b5e638ff89fc0981c3a885af314336c5effe7d
hostduong <hostduong@gmail.com>
2025-06-24 17:03:57 +0700

    Add files via upload

---
+++ public/js/front-config.js
@@ -0,0 +1,2 @@
+window.assetsPath=document.documentElement.getAttribute("data-assets-path");window.templateName=document.documentElement.getAttribute("data-template");window.rtlSupport=!0;
+//# sourceMappingURL=front-config.js.map
