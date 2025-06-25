b68443d00955e3ba1cf55704e59e330695d95130
hostduong <hostduong@gmail.com>
2025-06-25 19:45:06 +0700

    Delete public directory

--- public/js/front-config.js
+++
@@ -1,2 +0,0 @@
-window.assetsPath=document.documentElement.getAttribute("data-assets-path");window.templateName=document.documentElement.getAttribute("data-template");window.rtlSupport=!0;
-//# sourceMappingURL=front-config.js.map
