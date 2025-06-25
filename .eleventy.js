d2e7df12e1a533b593cf36149d99a819ef3573c8
hostduong <hostduong@gmail.com>
2025-06-25 22:37:59 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({"public/css": "css"});
-  eleventyConfig.addPassthroughCopy({"public/img": "img"});
-  eleventyConfig.addPassthroughCopy({"public/js": "js"});
+  // Copy toàn bộ build_fe sang build_fe (gốc)
+  eleventyConfig.addPassthroughCopy({ "build_fe": "build_fe" });
+  // Copy assets/img sang assets/img
+  eleventyConfig.addPassthroughCopy({ "assets/img": "assets/img" });
+  // Copy build sang build
+  eleventyConfig.addPassthroughCopy({ "build": "build" });
+  // Copy resources/assets/js sang resources/assets/js
+  eleventyConfig.addPassthroughCopy({ "resources/assets/js": "resources/assets/js" });
+
   return {
     dir: {
       input: "src",
