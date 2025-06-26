5dcc3e59efc992ccbc207b5f1c0b7c3c20369797
hostduong <hostduong@gmail.com>
2025-06-26 09:14:08 +0700

    Update register.ts

--- functions/api/register.ts
+++ functions/api/register.ts
@@ -1,4 +1,4 @@
-import { sha256, randomBase62 } from "../hash";
+import { sha256, randomBase62 } from "../hash.ts";
 // KV binding: env.KHOAI_KV_USER, env.KHOAI_KV_TOKEN, env.KHOAI_KV_COOKIE
 
 export async function onRequestPost(context) {
